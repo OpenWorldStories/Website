bc91479792853193badad2ec0dfe6eabce498cc2
theMichaelB <michael@openworldstories.com>
2021-11-17 16:52:56 +0000

    added mariadb database connection

--- docker/entrypoint.dev.sh
+++ docker/entrypoint.dev.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
+echo "Waiting for database..."
+
+while ! nc -z mariadb-server 3306; do
+    sleep 0.1
+done
+
+echo "database started"
+
+python manage.py makemigrations
+python manage.py migrate
+
 exec "$@"
--- ows/ows/settings.py
+++ ows/ows/settings.py
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'ows.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': os.environ.get("MARIADB_PASSWORD"),
+        'HOST': 'mariadb-server',
+        'PORT': '',
     }
 }
 
